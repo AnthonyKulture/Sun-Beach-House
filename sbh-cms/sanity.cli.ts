e703dbd0873f8fb3589a69d19f054e238e8794ee
AnthonyKulture <contact@kulturecom.fr>
2025-12-12 17:34:33 +0100

    Ajout de l'appId de déploiement Sanity

--- sbh-cms/sanity.cli.ts
+++ sbh-cms/sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'p8nz95i9dbra1f51ce53lttf',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
